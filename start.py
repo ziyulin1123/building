82d7b6d21c7dc1e2a653ae15b17b914dc502d915
ziyulin1123 <a0983984816@gmail.com>
2024-02-25 16:44:44 +0800

    python1

---
+++ start.py
@@ -0,0 +1,22 @@
+print("Hello Python")
+#字串 "用來表達任意文字"
+#有順序 可動列表list
+x=[3,4,5]
+print(x)
+["hello,world"]
+#有順序 不可變動tuple
+n=(3,4,5)
+print(n)
+("hello,world")
+#集合 set沒有順序之分
+{3,4,5}
+{"hello,world"}
+#字典 dictionary(鍵對值的配對)
+{"apple":"蘋果","cute":"可愛"}
+#變數:拿來儲存資料的自訂名稱
+#變數名稱=資料
+n=4
+print(n)
+#print(資料)
+print("aaa""bbb")
+print("www","doc","html",sep=".")
